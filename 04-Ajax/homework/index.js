e0357081febd03e641866110811cc926ad608bb2
DAbarboza <bardanii7@gmail.com>
2024-02-14 22:12:03 -0300

    act 14/02/2024

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const boton = document.getElementById("boton");
   boton.addEventListener("click", obtenerYMostrarAmigos);
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const botonBuscar = document.getElementById("search");
+  botonBuscar.addEventListener("click", buscarAmigoPorId);
+});
+
+//* FUNCIONES GET LISTA AMIGOS
 
 function obtenerYMostrarAmigos() {
   fetch("http://localhost:5000/amigos")
@@ -20,4 +26,25 @@ function agregarAmigoALista(amigo) {
   const li = document.createElement("li");
   li.textContent = amigo.name;
   lista.appendChild(li);
+}
+
+//* FUNCIONES MOSTRAR AMIGO POR ID
+
+function buscarAmigoPorId() {
+  const inputId = document.getElementById("idAmigo");
+  const id = inputId.value;
+
+  fetch(`http://localhost:5000/amigos/${id}`)
+    .then((response) => response.json())
+    .then(mostrarAmigoEnDOM);
+}
+
+function mostrarAmigoEnDOM(amigo) {
+  const amigoSpan = document.getElementById("amigo");
+  amigoSpan.textContent = `Nombre: ${amigo.nombre}, Edad: ${amigo.edad}`;
+}
+
+function mostrarResultadoBusqueda(amigo) {
+  const resultadoParrafo = document.getElementById("resultadoBusqueda");
+  resultadoParrafo.textContent = `Resultado de la búsqueda: Nombre: ${amigo.nombre}, Edad: ${amigo.edad}`;
 }
