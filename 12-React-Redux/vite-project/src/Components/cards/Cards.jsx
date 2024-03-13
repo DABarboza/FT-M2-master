18a839499c4ea02f59b29dbc7c78791a7a05db43
DAbarboza <bardanii7@gmail.com>
2024-03-12 22:58:52 -0300

    act 12/3/24

--- 12-React-Redux/vite-project/src/Components/cards/Cards.jsx
+++ 12-React-Redux/vite-project/src/Components/cards/Cards.jsx
@@ -1,9 +1,11 @@
+import Card from "../card/Card";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllUsers } from "../../redux/actions";
 const Cards = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.users);
+  console.log(state);
 
   useEffect(() => {
     dispatch(getAllUsers);
